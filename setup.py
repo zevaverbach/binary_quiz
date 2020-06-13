a43ce8de24b8766495f50a5cccabb9b079828aa2
zevav <zev@averba.ch>
2020-06-13 11:36:12 +0200

    ...

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name="binary_math_exercises",
     author="Zev Averbach",
     author_email="zev@averba.ch",
-    version="0.1",
+    version="0.1.1",
     license="MIT",
     long_description_content_type="text/markdown",
     description=(
